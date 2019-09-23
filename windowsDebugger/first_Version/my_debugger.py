bb04f96238fbf9f5ad4981d6aaf2744e489396c4
Masked Fox <iammaskedfx@gmail.com>
2019-09-23 11:56:30 -0400

    Implemented debug event handlers and exceptions

--- windowsDebugger/first_Version/my_debugger.py
+++ windowsDebugger/first_Version/my_debugger.py
@@ -10,14 +10,22 @@
 
 from ctypes import *
 from my_debugger_defines import *
+from my_debugger_defines import PROCESS_ALL_ACCESS, THREAD_ALL_ACCESS, \
+    PROCESS_INFORMATION, STARTUPINFO, DEBUG_PROCESS, DEBUG_EVENT, DBG_CONTINUE, INFINITE, THREADENTRY32, \
+    TH32CS_SNAPTHREAD, CONTEXT, CONTEXT_FULL, CONTEXT_DEBUG_REGISTERS
 
 kernel32 = windll.kernel32
 
-class debugger():
+
+class Debugger:
     def __init__(self):
-	self.h_process = None
-	self.pid = None
-	self.debugger_active = False
+        self.h_process = None
+        self.pid = None
+        self.debugger_active = False
+        self.h_thread = None
+        self.context = None
+        self.exception = None
+        self.exception_address = None
 
     def load(self, path_to_exe):
         # dwCreation flag determines how to create the process
@@ -25,98 +33,121 @@ class debugger():
         # to see the calculator GUI
         creation_flags = DEBUG_PROCESS
 
-        #instantiate the structs
-        startupinfo         = STARTUPINFO()
+        # instantiate the struct
+        startupinfo = STARTUPINFO()
         process_information = PROCESS_INFORMATION()
 
-        # THE FOllowing two opetions allow the started process
+        # THE Following two operations allow the started process
         # to be shown as a separate window. This also illustrates
         # hwo different settings in the STARTUPINFO struct can affect
-        # the debuggee.
-        startupinfo.dwFlags     = 0x1
+        # the debugger.
+        startupinfo.dwFlags = 0x1
         startupinfo.wShowWindow = 0x0
-        
-        # We then initialize the cb variable in the STARRUPINFO struct
+
+        # We then initialize the cb variable in the STARTUPINFO struct
         # which is just the size of the struct itself
         startupinfo.cb = sizeof(startupinfo)
 
         if kernel32.CreateProcessA(path_to_exe,
-                                    None,
-                                    None,
-                                    None,
-                                    None,
-                                    creation_flags,
-                                    None,
-                                    None,
-                                    byref(startupinfo),
-                                    byref(process_information)):
-		print("[*] We have successfully launched the process!")
-            	print("[*] PID: %d" % process_information.dwProcessId)
-	    	#Obtain a valid hanlde to the newly created process
-	    	#and store it for future access
-		self.h_process = self.open_process(process_information.dwProcessId)
+                                   None,
+                                   None,
+                                   None,
+                                   None,
+                                   creation_flags,
+                                   None,
+                                   None,
+                                   byref(startupinfo),
+                                   byref(process_information)):
+            print("[*] We have successfully launched the process!")
+            print("[*] PID: %d" % process_information.dwProcessId)
+            # Obtain a valid handle to the newly created process
+            # and store it for future access
+            self.h_process = self.open_process(process_information.dwProcessId)
 
         else:
             print("[*] Error: 0x%08x." % kernel32.GetLastError())
-    
-    def open_process(self, pid):
-	h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
-	return h_process
-  
-    def attach(self, pid):
-	self.h_process = self.open_process(pid)
-	#We attemp to attach to the process
-	#if this fails we exit the call
-	if kernel32.DebugActiveProcess(pid):
-		self.debugger_active = True
-		self.pid = int(pid)
-		
-	else:
-		print "[*] Unable to attach to the process."
-   
-    def run(self):
-	#Now we have to poll the debuggee for
-	#debugging events
-        while self.debugger_active == True:
-            self.get_debug_event()
-    
-    def get_debug_event(self):
-	debug_event = DEBUG_EVENT()
-	continue_status = DBG_CONTINUE
-	
-	if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
-		#We arent going to build any event handlers
-		# just yet. Lets just resume the process for now.
-		#raw_input("press a key to conitnue...")
-		#self.debugger_active=False
-		kernel32.ContinueDebugEvent(\
-			debug_event.dwProcessId, \
-			debug_event.dwThreadId, \
-			continue_status )
-		
-    def detach(self):
-	if kernel32.DebugActiveProcessStop(self.pid):
-		print("[*] Finished debugging. Exiting...")
-	else:
-		print("There wa an error")
-		return False
 
-    def open_thread(self, thread_id):
-	h_thread = kernel32.OpenThread(THREAD_ALL_ACCESS, None, thread_id)
-	
-	if h_thread is not None:
-		return h_thread
-	else:
-		print("[*] Could nto obtain a valid thread handle.")
-		return False
+    @staticmethod
+    def open_process(pid):
+        h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
+        return h_process
+
+    def attach(self, pid):
+        self.h_process = self.open_process(pid)
+        # We attempt to attach to the process
+        # if this fails we exit the call
+        if kernel32.DebugActiveProcess(pid):
+            self.debugger_active = True
+            self.pid = int(pid)
+
+        else:
+            print("[*] Unable to attach to the process.")
+
+    def run(self):
+        # Now we have to poll the debugger for
+        # debugging events
+        while self.debugger_active:
+            self.get_debug_event()
+
+    def get_debug_event(self):
+        debug_event = DEBUG_EVENT()
+        continue_status = DBG_CONTINUE
+        if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
+            # Let's obtain the thread and context information
+            self.h_thread = self.open_thread(debug_event.dwThreadId)
+            self.context = self.get_thread_context(h_thread = self.h_thread)
+            print("Event Code: %d Thread ID: %d" % (debug_event.dwDebugEventCode, debug_event.dwThreadId))
+            # If the event code is an exception, we want to
+            # examine it further.
+            if debug_event.dwDebugEventCode == EXCEPTION_DEBUG_EVENT:
+                # Obtain the exception code
+                exception = debug_event.u.Exception.ExceptionRecord.ExceptionCode
+                self.exception_address = debug_event.u.Exception.ExceptionRecord.ExceptionAddress
+
+                if exception == EXCEPTION_ACCESS_VIOLATION:
+                    print("Access Violation Detected.")
+                    # if a breakpoint is detected, we call an internal handler
+                elif exception == EXCEPTION_BREAKPOINT:
+                    continue_status = self.exception_handler_breakpoint()
+                elif exception == EXCEPTION_GUARD_PAGE:
+                    print("Guard Page Access Detected")
+                elif exception == EXCEPTION_SINGLE_STEP:
+                    print("Single Stepping.")
+            
+            kernel32.ContinueDebugEvent(
+                debug_event.dwProcessId,
+                debug_event.dwThreadId,
+                continue_status)
+
+    def exception_handler_breakpoint(self):
+        print("[*] Inside the breakpoint handler.")
+        print("Exception Address: 0x%08x" % self.exception_address)
+        return DBG_CONTINUE
+
+    def detach(self):
+        if kernel32.DebugActiveProcessStop(self.pid):
+            print("[*] Finished debugging. Exiting...")
+        else:
+            print("There wa an error")
+            return False
+
+    @staticmethod
+    def open_thread(thread_id):
+        h_thread = kernel32.OpenThread(THREAD_ALL_ACCESS, None, thread_id)
+
+        if h_thread is not None:
+            return h_thread
+        else:
+            print("[*] Could nto obtain a valid thread handle.")
+            return False
 
     def enumerate_threads(self):
-	thread_entry = THREADENTRY32()
-	thread_list = []
-	snapshot = kernel32.CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, self.pid)
+        thread_entry = THREADENTRY32()
+        thread_list = []
+        snapshot = kernel32.CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, self.pid)
 
-	if snapshot is not None:
-            #Yout have to set the size of the struct or the call will fail
+        if snapshot is not None:
+            # Your have to set the size of the struct or the call will fail
             thread_entry.dwSize = sizeof(thread_entry)
             success = kernel32.Thread32First(snapshot, byref(thread_entry))
 
@@ -127,19 +158,18 @@ class debugger():
 
             kernel32.CloseHandle(snapshot)
             return thread_list
-	else:
+        else:
             return False
 
     def get_thread_context(self, thread_id=None, h_thread=None):
-	context = CONTEXT()
-	context.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS
-	if not h_thread:
+        context = CONTEXT()
+        context.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS
+        if not h_thread:
             self.open_thread(thread_id)
-	# Obtain a handle to the thread
-	h_thread = self.open_thread(thread_id)
-	if kernel32.GetThreadContext(h_thread, byref(context)):
-		kernel32.CloseHandle(h_thread)
-		return context
-	else: 
-		return False
-
+        # Obtain a handle to the thread
+        h_thread = self.open_thread(thread_id)
+        if kernel32.GetThreadContext(h_thread, byref(context)):
+            kernel32.CloseHandle(h_thread)
+            return context
+        else:
+            return False
